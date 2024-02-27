83519e0185d690b2b0b5f3cd9446122329c44a15
afifi brahim <afifi71brahim@gmail.com>
2024-02-27 15:48:42 +0100

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './components/Buttons/Button'
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen font-Nunito '>
+      <Button size={'medium'} variant={'secondary'}>
+        Onclick
+      </Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cards = ({bgImage,title}) => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Cards
