c23aaccdd2f0bfa0214e92bcb3e06a4864d7fc08
afifi brahim <afifi71brahim@gmail.com>
2024-02-27 16:44:17 +0100

    add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Button from './components/Buttons/Button'
+import React from "react";
+import image1 from "./assets/villa.jpg";
+import image2 from "./assets/image1.jpg";
+import image3 from "./assets/image2.jpg";
+import Button from "./components/Buttons/Button";
+import Cards from "./components/Cards/Cards";
 const App = () => {
   return (
-    <div className='flex items-center justify-center h-screen font-Nunito '>
-      <Button size={'medium'} variant={'secondary'}>
+    <div className="flex items-center justify-center h-screen gap-4 font-Nunito bg-gray-300 ">
+      <Button size={"medium"} variant={"primary"}>
         Onclick
       </Button>
+      <Cards image={image1} />
+      <Cards image={image3} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import { GrStar } from "react-icons/gr";
+const Cards = ({image}) => {
+  // {bgImage,title,button}
 
-const Cards = ({bgImage,title}) => {
+  const backgroundImage = {};
+
   return (
-    <div>
-
-
+    <div className="p-2 rounded-2xl bg-white">
+      <img className="max-w-[300px] rounded-xl" src={image} alt="" />
+      <div className="flex justify-between mt-3">
+        <div>
+          <h3 className=" mb-2 font-bold">Fayrous Anaji</h3>
+          <p className="text-gray-400 text-sm mb-1">Casablanca</p>
+          <p>
+            {" "}
+            <span className="font-bold text-sm ">$200</span> a nuit{" "}
+          </p>
+        </div>
+        <div>
+           <GrStar className="text-orange-500"/>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
