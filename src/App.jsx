53aad9b57df00e33dd33363b5ae0773dc3cfa65e
afifi brahim <afifi71brahim@gmail.com>
2024-02-28 15:26:10 +0100

    add slide componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import image1 from "./assets/villa.jpg";
-import image2 from "./assets/image1.jpg";
-import image3 from "./assets/image2.jpg";
 import Cards from "./components/Cards/Cards";
+import Slide from './components/Slide/Slide.jsx'
 const App = () => {
   return (
     <div className=" min-h-screen flex flex-col  gap-4 font-Nunito bg-gray-300 ">
-      {/* <Cards image={image1} desc={'4-6 voyageurs: Logemenet entiere .5 Lits .3 Salles de bain Wifi.Cuisine. Parking Gratuit'} direction={'row'} width={'w-full'} country={'Rabat'} title={'Riad Villa'} stars={'4.99'} price={'300'} /> */}
-      <Cards image={image1} desc={''} direction={'col'} width={'max-w-[400px]'} country={'Marrakech'} title={'Villa Massira'} stars={'3.16'} price={'150'} />
+      {/* <Cards image={image1} desc={'4-6 voyageurs: Logemenet entiere .5 Lits .3 Salles de bain Wifi.Cuisine. Parking Gratuit'} direction={'row'} width={'full'} country={'Rabat'} title={'Riad Villa'} stars={'4.99'} price={'300'} /> */}
+
+      {/* <Cards image={image1} desc={''} direction={'default'} width={'default'} country={'Marrakech'} title={'Villa Massira'} stars={'3.16'} price={'150'} /> */}
+      <Slide/>
+      
     </div>
   );
 };
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,52 +1,33 @@
 import React from "react";
 import { GrStar } from "react-icons/gr";
 import { IoMdHeart } from "react-icons/io";
-const Cards = ({ image, desc, direction, width,country,title,stars,price }) => {
+const Cards = ({
+  image,
+  desc,
+  direction,
+  width,
+  country,
+  title,
+  stars,
+  price,
+}) => {
 
+  const widthStyle ={
+    full: 'w-full',
+    default:'max-w-[400px]'
+  }
+  const directionStyle ={
+    row: 'flex-row',
+    default:'flex-col'
+  }
 
+  const Styles = `${widthStyle[width]} ${directionStyle[direction]}`
+
+  
   return (
     <>
-      {/* this is a column card */}
-
-      {/* <div className="p-6 flex flex-row  gap-10  rounded-2xl bg-white">
-      <div className="relative">
-        <img className="max-w-[350px] rounded-xl" src={image} alt="" />
-      </div>
-
-      <div className="flex w-full justify-between items-start mt-3">
-        <div className="flex flex-col w-full  gap-4">
-          <div className="flex justify-between w-full">
-            <div className="w-full">
-              <p className="text-gray-400 text-sm ">Casablanca</p>
-              <h3 className="  font-bold">Villa Ana de luxe a Marrakech</h3>
-            </div>
-
-           <IoMdHeart className=" text-xl  text-red-500  cursor-pointer" />
-         </div>
-         <div className="w-10 bg-gray-300 h-[1px]  " />
-         <p className="text-gray-500">
-           4-6 voyageurs: Logemenet entiere .5 Lits .3 Salles de bain Wifi.
-           Cuisine. Parking Gratuit
-         </p>
-         <div className="w-10  bg-gray-300 h-[1px]  " />
-         <div className="flex w-full  items-center justify-between">
-           <div className="flex gap-1">
-             <span className="text-[12px] font-bold">4.91</span>
-             <GrStar className="text-orange-500" />{" "} <span className="text-sm text-gray-600">(318 evaluations)</span>
-            </div>
-            <div>
-            <span className="font-bold text-sm ">$200  </span> <span className="text-sm">/nuit</span>
-            </div>
-     </div>
-        </div>
-
-      </div>
-   </div> */}
-
-      {/* This is a row card */}
-
       <div
-        className={`p-6 flex flex-${direction} ${width} gap-10  rounded-2xl bg-white`}
+        className={`p-6 flex ${Styles}  gap-10  rounded-2xl bg-white`}
       >
         <div className="relative">
           <img className="max-w-[350px] rounded-xl" src={image} alt="" />
@@ -56,35 +37,36 @@ const Cards = ({ image, desc, direction, width,country,title,stars,price }) => {
           <div className="flex flex-col w-full  gap-4">
             <div className="flex justify-between items-start w-full">
               <div className="">
-                <p className="text-gray-400 text-sm ">{country}</p>
-                <h3 className="  font-bold">{title}</h3>
+                <p className="text-gray-400 text-[14px] text-sm ">{country}</p>
+                <h3 className=" text-[16px] font-bold">{title}</h3>
               </div>
 
-              {direction === "col" ? (
+              {direction === "default" ? (
                 <div className="flex  items-center gap-1">
-                  <span className="text-[12px] font-bold">{stars}</span>
-                  <GrStar className="text-orange-500" />{" "}
+                  <span className="text-[14px] font-[400]">{stars}</span>
+                  <GrStar className="text-orange-500 w-[16px] h-[16px]" />{" "}
                 </div>
               ) : (
                 <IoMdHeart className=" text-xl  text-red-500  cursor-pointer" />
               )}
             </div>
-            {desc && <div className="w-10 bg-gray-300 h-[1px]  " />}
-            {desc && <p className="text-gray-500">{desc}</p>}
-            {desc && <div className="w-10 bg-gray-300 h-[1px]  " />}
+            {desc && <div className="w-10 bg-gray-300 h-[2px]  " />}
+
+            {desc && <p className="text-gray-500 text-[14px]">{desc}</p>}
+            {desc && <div className="w-10 bg-gray-300 h-[2px]  " />}
             <div className="flex   items-center justify-between">
               {desc && (
                 <div className="flex gap-1">
-                  <span className="text-[12px] font-bold">{stars}</span>
-                  <GrStar className="text-orange-500" />{" "}
+                  <span className="text-[14px] font-[400] ">{stars}</span>
+                  <GrStar className="text-orange-500 w-[16px] h-[16px]"  />{" "}
                   <span className="text-sm text-gray-600">
                     (318 evaluations)
                   </span>
                 </div>
               )}
               <div>
-                <span className="font-bold text-sm ">${price} </span>{" "}
-                <span className="text-sm">/nuit</span>
+                <span className="font-extrabold text-[14px] ">${price} </span>{" "}
+                <span className="text-sm  text-gray-600">/nuit</span>
               </div>
             </div>
           </div>
